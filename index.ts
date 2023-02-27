e09e0b1e1e04b847efdb63ae62064984f6e61c59
Alexey Sokolov <alexvyber@gmail.com>
2023-02-27 13:25:23 +0300

    asdf

--- index.ts
+++ index.ts
@@ -1,6 +1,16 @@
 import fs from "node:fs";
 
-export function isDocker() {
+/**
+Check if the process is running inside a Docker container.
+@example
+```
+import { isDocker } from 'isdocker';
+if (isDocker()) {
+	console.log('Running inside a Docker container');
+}
+```
+*/
+export function isDocker(): boolean {
   try {
     fs.statSync("/.dockerenv");
     return true;
