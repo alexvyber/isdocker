10778c88004a87469262de7ca6e26bd692be1bdd
Alexey Sokolov <alexvyber@gmail.com>
2023-02-27 13:43:32 +0300

    new stuff

--- index.d.ts
+++ index.d.ts
@@ -3,8 +3,9 @@ Check if the process is running inside a Docker container.
 @example
 ```
 import { isDocker } from 'isdocker';
+
 if (isDocker()) {
-	console.log('Running inside a Docker container');
+	console.log('In Docker');
 }
 ```
 */
